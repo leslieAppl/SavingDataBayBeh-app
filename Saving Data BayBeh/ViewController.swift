304045becd898959a0262124505cbe67aca36257
Leslie <leslie.appl@gmail.com>
2019-10-01 14:58:52 +0800

    Initial Commit

---
+++ Saving Data BayBeh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Saving Data BayBeh
+//
+//  Created by leslie on 10/1/19.
+//  Copyright © 2019 leslie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
